1f3382e84c7c397afe05665f4028f1a687f19b64
okumurakengo <okumurakengo1a@gmail.com>
2019-03-03 21:19:58 +0900

    03 add

--- 03_sample/index.js
+++ 03_sample/index.js
@@ -3,15 +3,6 @@ const s = new server({ port: 5001 });
 
 s.on("connection", ws => {
     ws.on('message', message => {
-        // クライアントから受信
-        const { name, age } = JSON.parse(message);
-        console.log(name, age);
-
-        // クライアントへ送信
-        ws.send(JSON.stringify({
-            hoge: true,
-            fuga: [1, 3, 5],
-            piyo: 0.5,
-        }));
+        ws.send("hellor");
     });
 });
